bfff2cad514424bc07a526da4c20f6485ef70f11
Алексей <akarpushev@yandex.ru>
2024-06-06 16:23:20 +0300

    скобки

--- O-нотация. Простые структуры данных/Лабораторная 1/Скобочные группы/task.py
+++ O-нотация. Простые структуры данных/Лабораторная 1/Скобочные группы/task.py
@@ -5,7 +5,26 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: Входная строка для проверки
     :return: True, если последовательность корректна, False в противном случае
     """
-    ...  # TODO реализовать проверку скобочной группы
+    stack = 0
+    for i in brackets_row:
+        if i == '(':
+            stack += 1
+        elif i == ')':
+            stack -= 1
+        if stack < 0:
+            return False
+    else:
+        return False if stack !=0 else True
+
+
+
+    # while '()' in brackets_row:
+    #     text = brackets_row.replace('()', '')
+    #     return not text
+
+
+
+    # TODO реализовать проверку скобочной группы
 
 
 if __name__ == '__main__':
