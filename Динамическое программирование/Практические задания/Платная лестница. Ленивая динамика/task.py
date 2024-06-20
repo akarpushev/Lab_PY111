ae245193e003eb5f0c4802f27c36e66720a48c85
akarpushev <akarpushev@yandex.ru>
2024-06-20 19:52:50 +0300

    практика 4 20.06 практич задания

--- Динамическое программирование/Практические задания/Платная лестница. Ленивая динамика/task.py
+++ Динамическое программирование/Практические задания/Платная лестница. Ленивая динамика/task.py
@@ -10,8 +10,15 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: список целых чисел, где каждое целое число является стоимостью конкретной ступени
     :return: минимальная стоимость подъема на верхнюю ступень
     """
-    ...  # TODO реализовать ленивую динамику
+    # TODO реализовать ленивую динамику
 
+    @lru_cache
+    def rec_find(stairway, n):
+        if n == 0 or n == 1:
+            return stairway[n]
+        return stairway[n] + min(rec_find(stairway, n - 1), rec_find(stairway, n - 2))
+
+    return rec_find(stairway, len(stairway) -1)
 
 if __name__ == '__main__':
     print(stairway_path((1, 3, 1, 5)))  # 7
--- Динамическое программирование/Практические задания/Платная лестница. Прямой метод обхода/task.py
+++ Динамическое программирование/Практические задания/Платная лестница. Прямой метод обхода/task.py
@@ -9,7 +9,23 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: список целых чисел, где каждое целое число является стоимостью конкретной ступени
     :return: минимальная стоимость подъема на верхнюю ступень
     """
-    ...  # TODO реализовать прямой метод расчета
+    # TODO реализовать прямой метод расчета
+
+    count_stairs = len(stairway)
+
+    if count_stairs == 1:
+        return stairway[0]
+    if count_stairs == 2:
+        return stairway[1]
+
+    coast = [0] * count_stairs
+    coast[0] = stairway[0]
+    coast[1] = min(0, stairway[0]) + stairway[1]
+    for current_step in range(2, count_stairs):
+        coast[current_step] = min(coast[current_step -1], coast[current_step - 2]) + stairway[current_step]
+    #return coast
+    return coast[current_step]
+
 
 
 if __name__ == '__main__':
