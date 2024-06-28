ffeaee863348a975835a304c7f0ac040c92eb62d
Алексей <akarpushev@yandex.ru>
2024-06-28 19:32:13 +0300

    лаб. 3

--- Сортировки/Лабораторная 3/Сортировка подсчетами/task.py
+++ Сортировки/Лабораторная 3/Сортировка подсчетами/task.py
@@ -12,4 +12,28 @@ def sort(container: Sequence[int]) -> Sequence[int]:
     :param container: Массив, который надо отсортировать
     :return: Отсортированный в порядке возрастания массив
     """
-    ...  # TODO реализовать алгоритм сортировки подсчетами
+    # TODO реализовать алгоритм сортировки подсчетами
+    if not container:
+        return []
+
+    max_value = max(container)
+
+    # Шаг 1: Инициализируем массив count с размерами от 0 до max_value.
+    count = [0] * (max_value + 1)
+
+    # Шаг 2: Подсчитываем количество каждого элемента в исходном массиве.
+    for number in container:
+        count[number] += 1
+
+    # Шаг 3: Восстанавливаем отсортированный массив.
+    sorted_container = []
+    for number, freq in enumerate(count):
+        sorted_container.extend([number] * freq)
+
+    return sorted_container
+
+
+# Примеры использования
+if __name__ == '__main__':
+    example = [4, 2, 2, 8, 3, 3, 1, 0]
+    print(sort(example))  # Ожидаемый результат: [0, 1, 2, 2, 3, 3, 4, 8]
