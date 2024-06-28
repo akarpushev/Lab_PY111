962c4bf0c2d6754016b8e835127bf2b71d483d9d
Алексей <akarpushev@yandex.ru>
2024-06-28 19:17:49 +0300

    лаб. 2

--- Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный факториал/task.py
+++ Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный факториал/task.py
@@ -5,4 +5,18 @@ def factorial_iterative(n: int) -> int:
     :param n: Число, факториал которого нужно найти
     :return: n! - факториал числа n
     """
-    ...  # TODO реализовать итеративный алгоритм нахождения факториала
+    # TODO реализовать итеративный алгоритм нахождения факториала
+    if n < 0:
+        raise ValueError("Факториал определен только для неотрицательных чисел")
+
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+
+    return result
+
+# Примеры использования:
+# if __name__ == '__main__':
+#     print(factorial_iterative(5))  # 120
+#     print(factorial_iterative(0))  # 1
+#     print(factorial_iterative(10)) # 3628800
--- Итеративные и рекурсивные алгоритмы/Лабораторная 2/Рекурсивный факториал/task.py
+++ Итеративные и рекурсивные алгоритмы/Лабораторная 2/Рекурсивный факториал/task.py
@@ -1,3 +1,6 @@
+#import functools
+
+#@functools.lru_cache()
 def factorial_recursive(n: int) -> int:
     """
     Рассчитать факториал числа n рекурсивным способом
@@ -5,4 +8,22 @@ def factorial_recursive(n: int) -> int:
     :param n: Число, факториал которого нужно найти
     :return: n! - факториал числа n
     """
-    ...  # TODO реализовать рекурсивный алгоритм нахождения факториала
+    # TODO реализовать рекурсивный алгоритм нахождения факториала
+    if not isinstance(n, int):
+        raise TypeError("Факториал определен только для неотрицательных целых чисел")
+
+    if n < 0:
+        raise ValueError("Факториал определен только для неотрицательных целых чисел")
+
+    # Базовый случай: 0! = 1
+    if n == 0:
+        return 1
+
+    # Рекурсивный случай: n! = n * (n-1)!
+    return n * factorial_recursive(n - 1)
+
+#Примеры использования:
+if __name__ == '__main__':
+    print(factorial_recursive(5))  # 120
+    print(factorial_recursive(0))  # 1
+    print(factorial_recursive(10)) # 3628800
